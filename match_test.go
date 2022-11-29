ead5f6308bdbae03e4d5bbc2555b314036c2acad
Leslie <wqyuwss@gmail.com>
2022-11-28 22:40:46 -0800

    change to use fork organization

--- match_test.go
+++ match_test.go
@@ -6,8 +6,8 @@ import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/h2non/filetype/matchers"
-	"github.com/h2non/filetype/types"
+	"github.com/leslie-wang/filetype/matchers"
+	"github.com/leslie-wang/filetype/types"
 )
 
 func TestMatch(t *testing.T) {
